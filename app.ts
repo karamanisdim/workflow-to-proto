b4b1c3e1f91e33266000597960e903d83fcba529
karamanisj <karamanis@ceid.upatras.gr>
2022-01-27 14:26:48 +0200

    writestream

--- app.ts
+++ app.ts
@@ -7,15 +7,12 @@ console.log(jsonObj._id);
 const latestV: Number = jsonObj.latestVersion;
 console.log(latestV);
 
-class MyClass {
-
+class FileHandle {
     private fs = require('fs');
-
     constructor() { }
 
     createFile(fileName: string, input: string) {
-
-        this.fs.writeFile(fileName, input,  function(err: any) {
+        this.fs.writeFile(fileName, input, function (err: any) {
             if (err) {
                 return console.error(err);
             }
@@ -23,20 +20,35 @@ class MyClass {
         });
     }
 
-    showFile() {
-
-        this.fs.readFile('file.txt', function (err: any, data: { toString: () => string; }) {
+    showFile(fileName: string) {
+        this.fs.readFile(fileName, function (err: any, data: string) {
             if (err) {
                 return console.error(err);
             }
             console.log("Asynchronous read: " + data.toString());
         });
     }
+
+    writeFile(fileName: string, input: string) {
+        this.fs.writeFile(fileName, input,  {'flag':'a'},  function(err: any) {
+            if (err) {
+                return console.error(err);
+            }
+        });
+    }
 }
 
-// Usage
-var obj = new MyClass();
-obj.createFile("test.proto", "syntax = \"proto3\";");
-obj.showFile();
+// WriteStream used to append logs to file when file is ready
+// use {flags: 'a'} to append and {flags: 'w'} to erase and write a new file
+var logStream = fs.createWriteStream('test.proto', {flags: 'a'});
+logStream.write('\nInitial line...');
+logStream.write('\nInitial line...2');
+logStream.write('\nInitial line...3');
+logStream.end('\nthis is the end line');
 
 
+// writeFile used to append logs to file instantly
+var file = new FileHandle();
+file.createFile("test.proto", "syntax = \"proto3\";");
+file.writeFile("test.proto", "\nblablabla");
+file.showFile("test.proto");
